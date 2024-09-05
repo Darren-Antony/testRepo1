15e7576ef45cee7def2964a78a37033cc088230f
Darren <darren@gmail.com>
2024-09-05 11:17:52 +0530

    new commit

--- src/main/java/com/boot/greetingapp/GreetingappApplication.java
+++ src/main/java/com/boot/greetingapp/GreetingappApplication.java
@@ -12,17 +12,24 @@ public class GreetingappApplication {
 		SpringApplication.run(GreetingappApplication.class, args);
 	}
 
-	@GetMapping("/welcome")
+	@GetMapping("/home")
 	public String welcome() {
 		return "welcome to spring boot greeting app";
 	}
-	@GetMapping("/greeting")
+	
+	@GetMapping("/javacourse")
 	public String greeting() {
-		return "greetings from spring boot greeting app";
+		return "welcome to java course";
 	}
-	@GetMapping("/hello")
-	public String hello() {
-		return "hello from spring boot greeting app";
+	
+	@GetMapping("/test")
+	public String test() {
+		return "welcome to test page";
 	}
-
+	
+	@GetMapping("/exit")
+	public String bye() {
+		return "Thanks a lot see you again";
+	}
+	
 }
